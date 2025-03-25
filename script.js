01ce56d89b35b79f5597bf9e0e496550e10bf477
dhruvil isotiya <dhruvil012345@gmil.com>
2025-03-25 16:51:12 +0530

    edit key

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
-const inputbox=document.getElementById("input");
-const listTask=document.getElementById("list");
+const inputbox = document.getElementById("input");
+const listTask = document.getElementById("list");
 
-const shortcut =document.querySelector("add");
+const shortcut = document.querySelector("add");
 
 
-function addtask(){
-    if(inputbox.value !== ''){
-        let li=document.createElement("li");
-        li.innerHTML=inputbox.value;
-        listTask.appendChild(li);
-        let span=document.createElement("span");
-        span.innerHTML="\u00d7";
-        li.appendChild(span);  
-    }
-        
-    else{
+function addtask() {
+    if (inputbox.value == '') {
         alert("Please enteryour task");
     }
-        
-    
-    inputbox.value = "";
 
-    
+    let li = document.createElement("li");
+    li.innerHTML = inputbox.value;
+    listTask.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+
+    inputbox.value = "";
 }
-document.addEventListener('keydown',(ea)=>{
-    if(ea.shiftKey && ea.f){
+
+document.addEventListener('keydown', (ea) => {
+    if (ea.shiftKey && ea.shiftKey) {
         addtask();
     }
 });
 
-listTask.addEventListener("click",function(e){
-    if(e.target.tagName==="LI"){
+listTask.addEventListener("click", function (e) {
+    if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName==="SPAN"){
+    } else if (e.target.tagName === "SPAN") {
         e.target.parentElement.remove();
     }
 }, false);
 
-let isFullScreen=false;
+let isFullScreen = false;
 
 
